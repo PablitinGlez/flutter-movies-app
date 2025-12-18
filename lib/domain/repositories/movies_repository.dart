cde5608a434ab14092110d1eeda6b27b8cef68e2
Juan Pablo <gonzalez0421200@gmail.com>
2025-12-18 00:37:04 -0600

    Firs Commit

---
+++ lib/domain/repositories/movies_repository.dart
@@ -0,0 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class MoviesDatasource {
+  //no kiero crear instancias de ella
+  //este clasdefine que es lo que los metos que estas clases van a tener para traer esta data
+  //getnowplaingmovies
+
+  Future<List<Movie>> getNowPlaying({int page = 1});
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cinemapedia/router/app_router.dart';
+import 'package:cinemapedia/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme()
+    );
+  }
+}
